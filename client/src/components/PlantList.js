cb1067ec0037b9e5c38be83d512bd0ab2bc204a0
Nestor <ncervan87@gmail.com>
2020-08-07 10:51:02 -0700

    stretch goal

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -6,7 +6,9 @@ export default class PlantList extends Component {
   constructor() {
     super()
     this.state = {
-      plants: []
+      plants: [],
+      query: '',
+      filtered: []
     }
   }
   // when the component mounts:
@@ -22,10 +24,48 @@ export default class PlantList extends Component {
       })
   }
 
+  handleSearch = e => {
+    this.setState({
+      query: e.target.value
+    })
+    this.setState({ filtered: this.state.plants.filter(item => item.name.startsWith(this.state.query)) })
+  }
+
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
+
+    if (this.state.filtered.length > 0) {
+      return (
+        <main className="plant-list">
+          <input onChange={this.handleSearch} placeholder="search by name" type="text" />
+          {this.state?.filtered?.map((plant) => (
+            <div className="plant-card" key={plant.id}>
+              <img className="plant-image" src={plant.img} alt={plant.name} />
+              <div className="plant-details">
+                <h2 className="plant-name">{plant.name}</h2>
+                <p className="plant-scientific-name">{plant.scientificName}</p>
+                <p>{plant.description}</p>
+                <div className="plant-bottom-row">
+                  <p>${plant.price}</p>
+                  <p>☀️ {plant.light}</p>
+                  <p>💦 {plant.watering}x/month</p>
+                </div>
+                <button
+                  className="plant-button"
+                  onClick={() => this.props.addToCart(plant)}
+                >
+                  Add to cart
+                </button>
+              </div>
+            </div>
+          ))}
+        </main>
+      );
+    }
+
     return (
       <main className="plant-list">
+        <input onChange={this.handleSearch} placeholder="search by name" type="text" />
         {this.state?.plants?.map((plant) => (
           <div className="plant-card" key={plant.id}>
             <img className="plant-image" src={plant.img} alt={plant.name} />
