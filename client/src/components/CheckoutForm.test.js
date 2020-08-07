1107133be5c5fb3800c567133440f2d69a0a8cd4
Nestor <ncervan87@gmail.com>
2020-08-07 09:49:56 -0700

    add test to check if input is rendered, input fields work, submit button works

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,64 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import CheckoutForm from "./CheckoutForm";
+import React from "react"
+import { render, screen, fireEvent } from "@testing-library/react"
+import CheckoutForm from "./CheckoutForm"
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
 
-test("form shows success message on submit with form details", () => {});
+    const header = screen.getByText(/checkout form/i)
+    expect(header).toBeInTheDocument()
+});
+
+test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm />)
+
+    //test whether inputs are displayed on DOM
+    const fnInput = screen.getByLabelText(/first name/i)
+    expect(fnInput).toBeInTheDocument()
+
+    const lnInput = screen.getByLabelText(/last name/i)
+    expect(lnInput).toBeInTheDocument()
+
+    const addressInput = screen.getByLabelText(/address/i)
+    expect(addressInput).toBeInTheDocument()
+
+    const cityInput = screen.getByLabelText(/city/i)
+    expect(cityInput).toBeInTheDocument()
+
+    const stateInput = screen.getByLabelText(/state/i)
+    expect(stateInput).toBeInTheDocument()
+
+    const zipInput = screen.getByLabelText(/zip/i)
+    expect(zipInput).toBeInTheDocument()
+
+    //test if inputs are working
+    fireEvent.change(fnInput, { target: { value: 'John' } })
+    expect(fnInput.value).toBe('John')
+
+    fireEvent.change(lnInput, { target: { value: 'Doe' } })
+    expect(lnInput.value).toBe('Doe')
+
+    fireEvent.change(addressInput, { target: { value: '123 Fake St' } })
+    expect(addressInput.value).toBe('123 Fake St')
+
+    fireEvent.change(cityInput, { target: { value: 'Clearwater' } })
+    expect(cityInput.value).toBe('Clearwater')
+
+    fireEvent.change(stateInput, { target: { value: 'Utah' } })
+    expect(stateInput.value).toBe('Utah')
+
+    fireEvent.change(zipInput, { target: { value: '45600' } })
+    expect(zipInput.value).toBe('45600')
+
+    const submitBtn = screen.getByRole('button', { name: /checkout/i })
+    expect(submitBtn).toBeInTheDocument()
+
+    fireEvent.click(submitBtn)
+
+    const results = await screen.findByText('John Doe')
+    expect(results).toBeInTheDocument()
+
+
+})
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,16 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react'
+
+export const useForm = initialObj => {
+    const [values, setValues] = useState(initialObj)
+
+    const changeValue = (e) => {
+        const { name, value } = e.target
+        setValues({
+            ...values,
+            [name]: value
+        })
+    }
+
+    return [values, changeValue]
+}
